b28940e71e4c48b71d9af115cb8ce3d01d75958a
Adalberto Ramos Ribeiro <beto1821@pop-os.localdomain>
2022-06-06 20:44:54 -0300

    req04

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../RenderWithRouter';
+import App from '../App';
+import { NotFound } from '../pages';
+
+describe('Teste o componente <NotFound.js />', () => {
+  it('Teste se a página contém um heading h2 com o texto Page requested not found',
+    () => {
+      renderWithRouter(<NotFound />);
+      const headingEl = screen.getByRole('heading', {
+        name: /page requested not found/i,
+        level: 2,
+      });
+      expect(headingEl).toBeInTheDocument();
+    });
+  it('Teste se a página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif',
+    () => {
+      renderWithRouter(<NotFound />);
+      const imageNotFound = screen.getByRole('img', {
+        name: /pikachu crying because the page requested was not found/i,
+      });
+      expect(imageNotFound).toHaveProperty('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+    });
+});
