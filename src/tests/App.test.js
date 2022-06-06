ec69a29ceb50091a64bde21ba1c78be2ba21d688
Adalberto Ramos Ribeiro <beto1821@pop-os.localdomain>
2022-06-06 16:38:02 -0300

    req01 rev01

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,15 +1,31 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
-import renderWithRouter from '../components/util/RenderWithRouter';
+import renderWithRouter from '../RenderWithRouter';
 
-it('Teste o componente <App.js />', () => {
-  renderWithRouter(<App />);
-  const linkHome = screen.getByRole('link', { name: /home/i });
-  const linkAbout = screen.getByRole('link', { name: /about/i });
-  const linkFavorite = screen.getByRole('link', { name: /favorite pokémons/i });
+describe('Teste o componente <App.js />', () => {
+  it('Teste se o topo da aplicação contém um conjunto fixo de links de navegação:',
+    () => {
+      renderWithRouter(<App />);
+      const linkHome = screen.getByRole('link', { name: /home/i });
+      const linkAbout = screen.getByRole('link', { name: /about/i });
+      const linkFavorite = screen.getByRole('link', { name: /favorite pokémons/i });
 
-  expect(linkHome).toBeInTheDocument();
-  expect(linkAbout).toBeInTheDocument();
-  expect(linkFavorite).toBeInTheDocument();
+      expect(linkHome).toBeInTheDocument();
+      expect(linkAbout).toBeInTheDocument();
+      expect(linkFavorite).toBeInTheDocument();
+    });
+
+  it('Teste se redirecionada para a página inicial, na URL / clicar link Home;',
+    () => {
+      const { history } = renderWithRouter(<App />);
+      expect(history.location.pathname).toBe('/');
+    });
+  it('Teste se redirecionada para a página About, na URL / clicar link About;',
+    () => {
+      const { history } = renderWithRouter(<App />);
+      userEvent.click(screen.getByRole('link', { name: /about/i }));
+      expect(history.location.pathname).toBe('/about');
+    });
 });
