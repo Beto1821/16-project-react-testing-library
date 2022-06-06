fef0128ee64422e370d1c2bd6e18703388bf55e2
Adalberto Ramos Ribeiro <beto1821@pop-os.localdomain>
2022-06-06 15:49:10 -0300

    req01

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,15 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../components/util/RenderWithRouter';
+
+it('Teste o componente <App.js />', () => {
+  renderWithRouter(<App />);
+  const linkHome = screen.getByRole('link', { name: /home/i });
+  const linkAbout = screen.getByRole('link', { name: /about/i });
+  const linkFavorite = screen.getByRole('link', { name: /favorite pokémons/i });
+
+  expect(linkHome).toBeInTheDocument();
+  expect(linkAbout).toBeInTheDocument();
+  expect(linkFavorite).toBeInTheDocument();
+});
