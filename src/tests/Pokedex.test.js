067d9bc65e1efcb5dd3965a45e9d0f9253bb4f6c
Adalberto Ramos Ribeiro <beto1821@pop-os.localdomain>
2022-06-07 13:56:51 -0300

    req05 rev02

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -32,4 +32,37 @@ describe('Teste o componente <Pokedex.js />', () => {
     renderWithRouter(<App />);
     expect(screen.getByText(pokemons[0].name)).toBeInTheDocument();
   });
+
+  it('Teste se a Pokédex tem os botões de filtro', () => {
+    renderWithRouter(<App />);
+    const typeButtons = screen.getAllByTestId('pokemon-type-button');
+
+    typeButtons.forEach((button) => {
+      expect(button).toBeInTheDocument();
+      const typeBtn = screen.getByRole('button', { name: button.textContent });
+      userEvent.click(typeBtn);
+
+      const pokemonName = screen.getByTestId('pokemon-name');
+      const pokemonType = screen.getByTestId('pokemon-type');
+      const nextPokemon = screen.getByTestId('next-pokemon');
+
+      const pokemonsByType = pokemons.filter((pokemon) => (
+        pokemon.type === button.textContent
+      ));
+
+      pokemonsByType.forEach((pokemon) => {
+        expect(pokemon.type).toEqual(button.textContent);
+        expect(pokemonType.textContent).toEqual(pokemon.type);
+        expect(pokemonName.textContent).toEqual(pokemon.name);
+        userEvent.click(nextPokemon);
+      });
+    });
+  });
+
+  it('Teste se a Pokédex contém um botão para resetar o filtro', () => {
+    renderWithRouter(<App />);
+    const buttonAll = screen.getByRole('button', { name: /all/i });
+    expect(buttonAll).toBeInTheDocument();
+    userEvent.click(buttonAll);
+  });
 });
